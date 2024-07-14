934eb595342fff2e41bdde3a66799760b25733ec
Max <maksim.kostin57@mail.ru>
2024-07-15 01:46:42 +0300

    Добавление ещё 2 решений для легких задач

---
+++ zadanie_1572.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+
+    int n;
+    char choice;
+
+    do {
+        cout << "Введите размер матрицы: ";
+        cin >> n;
+        if (n < 1 || n > 100) {
+            cout << "Размер массива должен быть 1 <= n <= 100." << endl;
+            continue;
+        }
+
+        // Генерация матрицы
+        vector<vector<int>> mat(n, vector<int>(n));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                mat[i][j] = rand() % 100 + 1; // Случайные числа от 1 до 100
+            }
+        }
+
+        cout << "Сгенерированная матрица:\n";
+        for (const auto& row : mat) {
+            for (int val : row) {
+                cout << val << "\t";
+            }
+            cout << "\n";
+        }
+
+        // Вычисление суммы диагоналей
+        int sum = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += mat[i][i]; // Основная диагональ
+            if (i != n - i - 1) {
+                sum += mat[i][n - i - 1]; // Побочная диагональ
+            }
+        }
+
+        cout << "Сумма диагоналей: " << sum << endl;
+
+        cout << "Хотите попробовать другой массив? (1 - да / 0 - нет): ";
+        cin >> choice;
+
+    } while (choice == '1');
+
+    return 0;
+}
---
+++ zadanie_1748.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string input;
+    char choice;
+
+    do {
+        cout << "Введите элементы массива через пробел: ";
+        getline(cin, input);
+
+        stringstream s(input);
+        vector<int> nums;
+        int num;
+
+        int a = 0;
+        while (s >> num) {
+            if (num >= 1 && num <= 100) { // Проверка на ограничения
+                nums.push_back(num);
+                a++;
+            }
+            else {
+                cout << "Число должно быть в диапазоне от 1 до 100." << endl;
+                return 1;
+            }
+        }
+
+        // Подсчет частоты
+        int frequency[101] = { 0 };
+        for (int num : nums) {
+            frequency[num]++;
+        }
+
+        // Суммирование уникальных элементов
+        int sum = 0;
+        for (int i = 1; i <= 100; ++i) {
+            if (frequency[i] == 1) {
+                sum += i;
+            }
+        }
+
+        cout << "Сумма уникальных элементов: " << sum << endl;
+
+        cout << "Хотите попробовать другой массив? (1 - да / 0 - нет): ";
+        cin >> choice;
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    } while (choice == '1');
+
+    return 0;
+}
